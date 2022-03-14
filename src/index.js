cb040c2022c800bdca4e682bb8d62d35ac4163b5
Juan David De la hoz <hozjuanda11@gmail.com>
2022-03-13 22:35:56 -0500

    API-REST

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 // import { init } from './js/chistes-page'
-import { init } from './js/usuarios-page';
+// import { init } from './js/usuarios-page';
+// init();
 
-init();
+import * as CRUD from './js/crud-provider';
+
+
+// CRUD.getUsuario( 2 ).then( console.log );
+
+// CRUD.crearUsuario( {
+//     name: 'Juan',
+//     job: 'Ingeniero'
+// } ).then( console.log );
+
+// CRUD.actualizarUsuario( 1, {
+//     name: 'Tomas',
+//     job: 'Desarrollador'
+// } ).then( console.log );
+
+CRUD.borrarUsuario( 1 ).then( console.log );
+
+
